2194acb51853dd8bd4336f6ef3c84962ba30c917
OriLapushner <ori.lapushner@gmail.com>
2018-04-03 16:18:37 +0300

    cleaned unused code

--- server-full.js
+++ server-full.js
@@ -1,10 +1,4 @@
-// Minimal Simple REST API Handler (With MongoDB and Socket.io)
-// Plus support for simple login and session
-// Plus support for file upload
-// Author: Yaron Biton misterBIT.co.il
-
 "use strict";
-
 var cl = console.log;
 
 const express = require('express'),
@@ -34,7 +28,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(clientSessions({
 	cookieName: 'session',
-	secret: 'C0d1ng 1s fun 1f y0u kn0w h0w', // set this to a long random string!
+	secret: 'ASDIsadi391243mxcvasdpoqxcvleus waiqpe933.x', //random string
 	duration: 30 * 60 * 1000,
 	activeDuration: 5 * 60 * 1000,
 }));
@@ -47,8 +41,7 @@ function dbConnect() {
 
 	return new Promise((resolve, reject) => {
 		// Connection URL
-		// var url = 'mongodb://mlab.com:35917/instadb';
-		var url = 'mongodb://localhost:27017/instaDb';
+		var url = 'mongodb://localhost:27017/chessdb';
 		// Use connect method to connect to the Server
 		mongodb.MongoClient.connect(url, function (err, db) {
 			if (err) {
@@ -65,57 +58,27 @@ function dbConnect() {
 
 
 
-// Basic Login/Logout/Protected assets
+// Basic Login
 app.post('/login', function (req, res) {
 	console.log(req.body)
 	dbConnect().then((db) => {
-		db.collection('user').findOne({ username: req.body.username, pass: req.body.pass }, function (err, user) {
+		db.collection('users').findOne({ username: req.body.username, pass: req.body.pass }, function (err, user) {
 			if (user) {
-				cl('Login Succesful');
-				delete user.pass;
-				req.session.user = user;
-				console.log('sending user: ', user)
-				res.json({ token: '', user });
-				db.collection('story').find({}).toArray((err, objs) => {
-					if (err) {
-						cl('Cannot get you a list of ', err)
-					} else {
-						cl("Returning list of " + objs.length + " stories");
-						io.emit('feedSend', objs);
-					}
-					db.close();
-				});
-			} else {
-				cl('Login NOT Succesful');
-				req.session.user = null;
-				res.json(403, { error: 'Login failed' })
+				console.log(user ,' has connected')
 			}
-		});
 	});
 });
+})
 
+//logout
 app.get('/logout', function (req, res) {
 	req.session.reset();
 	res.end('Loggedout');
 });
 
-function requireLogin(req, res, next) {
-	if (!req.session.user) {
-		cl('Login Required');
-		res.json(403, { error: 'Please Login' })
-	} else {
-		next();
-	}
-}
-
-app.get('/protected', requireLogin, function (req, res) {
-	res.end('User is loggedin, return some data');
-});
 
 
-// Kickup our server 
-// Note: app.listen will not work with cors and the socket
-// app.listen(3003, function () {
+
 http.listen(port, function () {
 	console.log(`misterREST server is ready at ${baseUrl}`);
 	console.log(`GET (list): \t\t ${baseUrl}/{entity}`);
@@ -125,25 +88,6 @@ http.listen(port, function () {
 	console.log(`POST (add): \t\t ${baseUrl}/{entity}`);
 
 });
-
-
-// Some small time utility functions
-
-
-
-
-// function cl(...params) {
-// 	console.log.apply(console, params);
-// }
-
-// Just for basic testing the socket
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/test-socket.html');
-// });
-
-
-
-
 
 
 
